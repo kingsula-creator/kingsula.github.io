369bed20f3ad5b25ff017c7e0a3a2a12732f3337
kingsula-creator <arisulaeman1@gmail.com>
2026-09-14 18:40:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // ================================
 // KINGSULA BLOG - SCRIPT
 // ================================
+// ================================
+// JUMLAH PENGUNJUNG
+// ================================
+async function tampilkanPengunjung() {
+  try {
+    const res = await fetch("https://api.countapi.xyz/hit/kingsula-blog/beranda");
+    const data = await res.json();
+    
+    const elemen = document.getElementById("jumlahPengunjung");
+    if (elemen) {
+      elemen.textContent = data.value;
+    }
+  } catch (err) {
+    console.log("Gagal ambil data pengunjung");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", tampilkanPengunjung);
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
